7061318d00de36ebbebc49877138ea099e0a5aa5
jglee73 <71472586+jglee73@users.noreply.github.com>
2024-09-20 11:09:35 +0900

    update licence
    update licence

--- Linker_Path.bat
+++
@@ -1,5 +0,0 @@
-@echo on
-
-setx path "C:\Module_Linker\EQP_Link_Res\EQP_VER;C:\Module_Linker\EQP_Link_Res-x64\EQP_VER;C:\Module_Linker\SUI_Link_Res\SUI_VER;%programfiles%;%programfiles(x86)%;%systemroot%;"
-
-pause
